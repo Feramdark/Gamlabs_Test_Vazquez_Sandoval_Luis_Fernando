9dcc6ac8d2f2e27ce1520ca5f3de08c6cbfb241e
Feramdark_0898 <70911659+Feramdark@users.noreply.github.com>
2021-07-29 14:42:00 -0500

    problema 2_100%

--- problema_2.php
+++ problema_2.php
@@ -1,26 +1,37 @@
 <?php
 
+
 $arreglo = array();
+$apuntador = 1;
 $mayor = 0;
 
 for ($i = 2; $i <= 104743 ; $i++) { 
-    if (numero_primo($i) && $i>$mayor) {
+    if (numero_primo($i)) {
         //$mayor = $i;
-        //echo "El numero ". $mayor . " es primo <br>";
-        array_push($arreglo,$i);
+        //echo "El numero ". $i . " es primo <br>";
+        $arreglo[$apuntador] = $i;
+        $apuntador++;
     }
 }
+//Recorre el arreglo e indica la posicion en la cual se encuentran los elementos
 
-
-//print_r($arreglo);
-for ($n=0; $n < count($arreglo) ; $n++) { 
-    echo "Posicion: ".$arreglo[$n]. "<br>";
+for ($n=1; $n <= count($arreglo) ; $n++) { 
+    //echo "Pos: ".$n." Valor: ".$arreglo[$n]. "<br>";  -> descomentar para observar la lista
 }
 
+$numMayor = $arreglo[1];
+$index = 0;
+for ($i = 1; $i <= count($arreglo) ; $i++) { 
+    if ($arreglo[$i] > $numMayor) {
+       $numMayor = $arreglo[$i];
+       $index = $i;
+
+    }
+}
+echo "El número mayor es ".$numMayor ." su posicion es ". $index;
 
 
-
-
+// conoce si tiene uno o mas divisores
 function numero_primo($numero){
     $cont = 0;
 
@@ -33,4 +44,3 @@ function numero_primo($numero){
     }
     return true;
 }
-
