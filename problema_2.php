8f25640dc0f8d8f2ffb9468669a54e430acd8a48
Feramdark_0898 <70911659+Feramdark@users.noreply.github.com>
2021-07-29 13:15:30 -0500

    problema2_75%

--- problema_2.php
+++ problema_2.php
@@ -1,31 +1,36 @@
 <?php
 
-$posicion = 10001;
-$numPrims = [2];
-$valor = 3;
-$divisor = 2;
+$arreglo = array();
+$mayor = 0;
 
-$found = false;
-
-
-while (count($numPrims) < $posicion) {
-    while ($divisor < $valor) {
-        if ($valor%$divisor == 0) {
-            $found = false;
-            break;
-        }
-        else{
-            $found = true;
-            $divisor += 1;
-        }
+for ($i = 2; $i <= 104743 ; $i++) { 
+    if (numero_primo($i) && $i>$mayor) {
+        //$mayor = $i;
+        //echo "El numero ". $mayor . " es primo <br>";
+        array_push($arreglo,$i);
     }
-    if ($found) {
-        $found = false;
-        array_push($numPrims,$valor);
-    }
-    $valor += 1;
-    $divisor = 2;
-    echo $numPrims[$posicion];
 }
 
+
+//print_r($arreglo);
+for ($n=0; $n < count($arreglo) ; $n++) { 
+    echo "Posicion: ".$arreglo[$n]. "<br>";
+}
+
+
+
+
+
+function numero_primo($numero){
+    $cont = 0;
+
+    for ($j=2; $j <= $numero ; $j++) { 
+        if ($numero % $j == 0) {
+            if (++$cont>1) { //incrementa y luego regresa su valor
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
