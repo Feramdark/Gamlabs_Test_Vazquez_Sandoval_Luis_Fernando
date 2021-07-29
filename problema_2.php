25febc481594bc2ef5d5c2e7be88447b7f6ee0cf
Feramdark_0898 <70911659+Feramdark@users.noreply.github.com>
2021-07-29 10:58:06 -0500

    Problema_1 _100% _ Problema_2_ 75%

--- problema_2.php
+++ problema_2.php
@@ -1,9 +1,31 @@
 <?php
 
-$arreglo=[];
+$posicion = 10001;
+$numPrims = [2];
+$valor = 3;
+$divisor = 2;
 
-for ($i=0; $i <10 ; $i++) { 
-    $arreglo[$i] = $i;
+$found = false;
+
+
+while (count($numPrims) < $posicion) {
+    while ($divisor < $valor) {
+        if ($valor%$divisor == 0) {
+            $found = false;
+            break;
+        }
+        else{
+            $found = true;
+            $divisor += 1;
+        }
+    }
+    if ($found) {
+        $found = false;
+        array_push($numPrims,$valor);
+    }
+    $valor += 1;
+    $divisor = 2;
+    echo $numPrims[$posicion];
 }
 
-var_dump($arreglo); 
+
